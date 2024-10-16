82ff38f3c65e11d4ac063d943bb48cb2e82646d5
PhamBaoAnh <baoanhp46@gmail.com>
2024-10-16 11:33:39 +0700

    login

--- lib/features/authentication/screens/signup/signup.dart
+++ lib/features/authentication/screens/signup/signup.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:project/features/authentication/screens/signup/widgrts/signup_form.dart';
+import 'package:project/utils/constants/colors.dart';
+import '../../../../common/widgets/login_signup/form_divider.dart';
+import '../../../../common/widgets/login_signup/social_buttons.dart';
 import '../../../../utils/constants/sizes.dart';
 import '../../../../utils/constants/text_strings.dart';
+import '../../../../utils/helpers/helper_functions.dart';
 
 class SignupScreen extends StatelessWidget {
   const SignupScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+
+
     return Scaffold(
       appBar: AppBar(
         title: const Text(TTexts.signupTitle), // Added title to the AppBar
@@ -21,22 +29,13 @@ class SignupScreen extends StatelessWidget {
               Text(TTexts.signupTitle, style: Theme.of(context).textTheme.headlineMedium),
               const SizedBox(height: TSizes.defaultSpace), // Add spacing below the title
 
-              Form(child: Column(
-                children: [
-                  Row(
-                    children: [
-                      Expanded(
-                        child: TextFormField(
-                          decoration: const InputDecoration(
-                            labelText: TTexts.firstName,
-                            prefixIcon: Icon(Iconsax.user),
-                          ),
-                        ),
-                      )
-                    ],
-                  )
-                ],
-              ))
+              const TSignupForm(),
+              const SizedBox(height: TSizes.spaceBtwSections),
+
+              TFormDivider(dividerText: TTexts.orSignInWith.capitalize!),
+              const SizedBox(height: TSizes.spaceBtwSections),
+
+              const TSocialButtons(),
             ],
           ),
         ),
@@ -44,3 +43,5 @@ class SignupScreen extends StatelessWidget {
     );
   }
 }
+
+
