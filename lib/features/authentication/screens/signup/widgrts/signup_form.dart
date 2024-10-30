4ae8c0f8c48c9ea291ed6745beab6df82cf9f936
sondoansda <vanson21102003@gmail.com>
2024-10-30 19:23:24 +0700

    validate sign up

--- lib/features/authentication/screens/signup/widgrts/signup_form.dart
+++ lib/features/authentication/screens/signup/widgrts/signup_form.dart
@@ -1,116 +1,186 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:project/features/authentication/controllers.onboarding/signup_controller.dart';
 import 'package:project/features/authentication/models/user_model.dart';
 import 'package:project/features/authentication/screens/signup/widgrts/term_condition_textbox.dart';
-import 'package:project/repository/auth_repo/AuthenticationRepository.dart';
 import '../../../../../utils/constants/sizes.dart';
 import '../../../../../utils/constants/text_strings.dart';
 
 class TSignupForm extends StatelessWidget {
   const TSignupForm({
     super.key,
-
   });
+
   @override
   Widget build(BuildContext context) {
     final controller = Get.put(SignupController());
-    final _formKey = GlobalKey<FormState>();
+    final formKey = GlobalKey<FormState>();
+
     return Form(
-        key: _formKey,
+        key: formKey,
         child: Column(
-      children: [
-        Row(
           children: [
-            Expanded(
-              child: TextFormField(
-                controller: controller.firstName,
-                decoration: const InputDecoration(
-                  labelText: TTexts.firstName,
-                  prefixIcon: Icon(Iconsax.user),
+            Row(
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    controller: controller.firstName,
+                    validator: (String? value) {
+                      return (value == null || value.isEmpty)
+                          ? 'Please enter your first name.'
+                          : null;
+                    },
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    decoration: const InputDecoration(
+                      labelText: TTexts.firstName,
+                      prefixIcon: Icon(Iconsax.user),
+                      errorStyle: TextStyle(
+                        color: Colors.red,
+                        fontSize: 12,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: TSizes.spaceBtwInputFields),
+                Expanded(
+                  child: TextFormField(
+                    controller: controller.lastName,
+                    validator: (value) {
+                      return (value == null || value.isEmpty)
+                          ? 'Please enter your last name.'
+                          : null;
+                      return null;
+                    },
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    decoration: const InputDecoration(
+                      labelText: TTexts.lastName,
+                      prefixIcon: Icon(Iconsax.user),
+                      errorStyle: TextStyle(
+                        color: Colors.red,
+                        fontSize: 12,
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+            const SizedBox(height: TSizes.spaceBtwSections),
+            TextFormField(
+              controller: controller.username,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter user name.';
+                }
+
+                return null;
+              },
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              expands: false,
+              decoration: const InputDecoration(
+                labelText: TTexts.username,
+                prefixIcon: Icon(Iconsax.user_edit),
+                errorStyle: TextStyle(
+                  color: Colors.red,
+                  fontSize: 12,
                 ),
               ),
             ),
-            const SizedBox(width: TSizes.spaceBtwInputFields),
-            Expanded(
-
-              child: TextFormField(
-                controller: controller.lastName,
-                decoration: const InputDecoration(
-                  labelText: TTexts.lastName,
-                  prefixIcon: Icon(Iconsax.user),
+            const SizedBox(height: TSizes.spaceBtwSections),
+            TextFormField(
+              controller: controller.email,
+              expands: false,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter your email';
+                }
+                final emailRegex = RegExp(r'^[^@]+@[^@]+\.[^@]+');
+                if (!emailRegex.hasMatch(value)) {
+                  return 'Email is not valid.';
+                }
+                return null;
+              },
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              decoration: const InputDecoration(
+                labelText: TTexts.email,
+                prefixIcon: Icon(Iconsax.direct),
+                errorStyle: TextStyle(
+                  color: Colors.red,
+                  fontSize: 12,
                 ),
               ),
-            )
+            ),
+            const SizedBox(height: TSizes.spaceBtwSections),
+            TextFormField(
+              controller: controller.phoneNo,
+              expands: false,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter your phone.';
+                }
+                final phoneRegex = RegExp(r'^\+84\d{9,10}$');
+                if (!phoneRegex.hasMatch(value)) {
+                  return 'Phone is not valid.';
+                }
+                return null; // Email hợp lệ
+              },
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              decoration: const InputDecoration(
+                labelText: TTexts.phoneNo,
+                prefixIcon: Icon(Iconsax.call),
+                errorStyle: TextStyle(
+                  color: Colors.red,
+                  fontSize: 12,
+                ),
+              ),
+            ),
+            const SizedBox(height: TSizes.spaceBtwSections),
+            TextFormField(
+              controller: controller.password,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter your password';
+                }
+                if (value.length <= 6) {
+                  return 'Your password must be more than 6 characters.';
+                }
+                return null; // Email hợp lệ
+              },
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              obscureText: true,
+              decoration: const InputDecoration(
+                labelText: TTexts.password,
+                prefixIcon: Icon(Iconsax.password_check),
+                suffixIcon: Icon(Iconsax.eye_slash),
+                errorStyle: TextStyle(
+                  color: Colors.red,
+                  fontSize: 12,
+                ),
+              ),
+            ),
+            const SizedBox(height: TSizes.spaceBtwSections),
+            const TTermAndConditionTextBox(),
+            const SizedBox(height: TSizes.spaceBtwSections),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                  onPressed: () {
+                    if (formKey.currentState!.validate()) {
+                      final userModel = UserModel(
+                          firstName: controller.firstName.text.trim(),
+                          lastName: controller.lastName.text.trim(),
+                          username: controller.username.text.trim(),
+                          email: controller.email.text.trim(),
+                          password: controller.password.text.trim(),
+                          phoneNo: controller.phoneNo.text.trim());
+                      SignupController.instance.createUser(userModel);
+                    }
+                  },
+                  child: const Text(
+                    TTexts.createAccount,
+                  )),
+            ),
           ],
-        ),
-        const SizedBox(height: TSizes.spaceBtwSections),
-        TextFormField(
-          controller: controller.username,
-          expands: false,
-          decoration: const InputDecoration(
-            labelText: TTexts.username,
-            prefixIcon: Icon(Iconsax.user_edit),
-          ),
-
-        ),
-        const SizedBox(height: TSizes.spaceBtwSections),
-        TextFormField(
-          controller: controller.email,
-          expands: false,
-          decoration: const InputDecoration(
-            labelText: TTexts.email,
-            prefixIcon: Icon(Iconsax.direct),
-          ),
-
-        ),
-        const SizedBox(height: TSizes.spaceBtwSections),
-        TextFormField(
-          controller: controller.phoneNo,
-          expands: false,
-          decoration: const InputDecoration(
-            labelText: TTexts.phoneNo,
-            prefixIcon: Icon(Iconsax.call),
-          ),
-
-        ),
-        const SizedBox(height: TSizes.spaceBtwSections),
-        TextFormField(
-          controller: controller.password,
-          obscureText: true,
-          decoration: const InputDecoration(
-            labelText: TTexts.password,
-            prefixIcon: Icon(Iconsax.password_check),
-            suffixIcon:Icon(Iconsax.eye_slash),
-          ),
-
-        ),
-        const SizedBox(height: TSizes.spaceBtwSections),
-
-         const TTermAndConditionTextBox(),
-        const SizedBox(height: TSizes.spaceBtwSections),
-        SizedBox(width: double.infinity,
-          child: ElevatedButton(
-              onPressed:
-                  // () => Get.to(() => const VerifyEmailScreen()),
-              (){
-                 if(_formKey.currentState!.validate()){
-                   final userModel = UserModel(firstName: controller.firstName.text.trim(), lastName:controller.lastName.text.trim(), username:controller.username.text.trim(), email: controller.email.text.trim(), password:controller.password.text.trim(), phoneNo: controller.phoneNo.text.trim());
-                    SignupController.instance.createUser(userModel);
-                 }
-
-                 },
-              child: const Text(
-                TTexts.createAccount,
-              )
-          ),
-        ),
-
-      ],
-    ));
+        ));
   }
 }
-
