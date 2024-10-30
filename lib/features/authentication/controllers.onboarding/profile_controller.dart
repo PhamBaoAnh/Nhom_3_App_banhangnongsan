2b95986d45f439009680ec624a11025fbe4f153e
sondoansda <vanson21102003@gmail.com>
2024-10-30 09:00:57 +0700

    login logout signup signup-gg

---
+++ lib/features/authentication/controllers.onboarding/profile_controller.dart
@@ -0,0 +1,31 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get/get.dart';
+import 'package:project/features/authentication/models/user_model.dart';
+import 'package:project/repository/auth_repo/AuthenticationRepository.dart';
+import 'package:project/repository/user_repo/user_repo.dart';
+
+class ProfileController extends GetxController {
+  static ProfileController get instance => Get.find();
+  final _authRepo = Get.put(AuthenticationRepository());
+  final _userRepo = Get.put(userRepo());
+
+  getUserData() async {
+    final email = _authRepo.firebaseUser?.email;
+    if (email != null) {
+      UserModel userData = await _userRepo.getUserDetail(email);
+      return userData;
+    } else {
+      Get.snackbar('Error', "Login to continue");
+      return null;
+    }
+  }
+
+  updateUserData(UserModel user) async {
+   await  _userRepo.getUpdateUser(user);
+
+  }
+  logOut() async {
+     await _authRepo.logOut();
+  }
+
+}
